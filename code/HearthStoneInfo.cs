daa2dfe606253644c29f98f531a64582dcc18659
Drew Knab <drewknab@gmail.com>
2019-12-29 15:24:16 -0500

    Initial Commit

---
+++ code/HearthStoneInfo.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+namespace BlazorHearthstone
+{
+    public class Information
+    {
+        public string patch { get; set; }
+        public string[] classes { get; set; }
+        public string[] sets { get; set; }
+        public string[] standard { get; set; }
+        public string[] wild { get; set; }
+        public string[] types { get; set; }
+        public string[] factions { get; set; }
+        public string[] qualities { get; set; }
+        public string[] races { get; set; }
+    }
+}
